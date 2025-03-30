f0feadf931b7c8e81c8f83840ca9497f3765bc90
hyebinrk <gpqls3698@naver.com>
2025-03-31 03:34:04 +0900

    250331

--- toyproject/madang_작업쿼리.sql
+++ toyproject/madang_작업쿼리.sql
@@ -6,6 +6,7 @@ SELECT * FROM Students;
 -- 더미데이터 삽입
 INSERT INTO Students(std_id, std_name, std_mobile, std_regyear)
 VALUES (SEQ_STUDENT.nextval, '홍길동', '010-9999-8888', 1997);
+
 INSERT INTO Students(std_id, std_name, std_mobile, std_regyear)
 VALUES (SEQ_STUDENT.nextval, '홍길순', '010-9999-8877', 2000);
 
