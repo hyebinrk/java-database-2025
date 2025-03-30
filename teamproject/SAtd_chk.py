1ead091587d1814cd8ed17fe8159eba7b72965a9
hyebinrk <gpqls3698@naver.com>
2025-03-31 03:19:45 +0900

    250331

---
+++ teamproject/SAtd_chk.py
@@ -0,0 +1,170 @@
+# 시스템 모듈 임포트
+import sys
+
+# PyQt5 위젯 및 GUI 관련 모듈 임포트
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import QtGui, QtWidgets, uic
+
+# Oracle 데이터베이스 연결을 위한 cx_Oracle 모듈 임포트
+import cx_Oracle as oci
+
+# Oracle 데이터베이스 연결 정보 설정
+sid = 'XE'  # 데이터베이스 SID
+host = 'localhost'  # 데이터베이스 호스트 주소 (외부 접속 시 변경 필요)
+port = 1521  # 데이터베이스 포트 번호
+username = 'attendance'  # 데이터베이스 사용자 이름
+password = '12345'  # 데이터베이스 비밀번호
+basic_msg = 'OO고등학교 출결관리앱 v1.0'
+
+# 학생 로그인 > 출결체크 화면
+class SAtdMainWindow(QMainWindow):
+    def __init__(self, s_id=None):
+        super(SAtdMainWindow, self).__init__()
+        self.s_id = s_id # 로그인 클래스에서 전달받은 S_ID 저장 
+        self.initUI()
+        
+
+    def initUI(self):
+        uic.loadUi('./SAtd_chk.ui', self)
+        self.setWindowTitle('학생용출결체크')
+        self.setWindowIcon(QIcon('./image/app01.png'))
+        
+        self.btn_my.clicked.connect(self.MypageWindow)
+        self.btn_atd.clicked.connect(self.MgmtAtdWindow)
+        self.btn_eal.clicked.connect(self.btnEalClick)
+        self.btn_cob.clicked.connect(self.btnCobClick)
+        self.btn_out.clicked.connect(self.btnOutClick)
+       
+    def MypageWindow(self):
+        from mypage import MypageWindow as MypageWindow  # 마이페이지.py에서 MypageWindow 가져오기
+        self.my_page = MypageWindow()  # T_ID 전달
+        self.my_page.show()  # 출결 체크 창 열기
+
+    def MgmtAtdWindow(self):
+        from AttendanceApp import AttendanceApp
+        self.atd_mgmt = AttendanceApp()  # T_ID 전달
+        self.atd_mgmt.show()
+
+
+    
+    def btnEalClick(self):
+        """조퇴 버튼 클릭 시 DB 저장"""
+        try:
+            connection = oci.connect(username, password, f"{host}:{port}/{sid}")
+            cursor = connection.cursor()
+
+            cursor.execute("SELECT s_no FROM student WHERE s_id = :s_id", {"s_id": self.s_id})
+            result = cursor.fetchone()
+
+            if result:
+                s_no = result[0]  # 학생 번호 가져오기
+
+                query = """UPDATE atd
+                        SET leave_time = CAST(SYSTIMESTAMP AT TIME ZONE 'UTC' AS TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'Asia/Seoul'
+                        WHERE s_no = :s_no
+                        AND trunc(atd_date) = trunc(sysdate)"""
+                
+
+                cursor.execute(query, {"s_no": s_no})
+                connection.commit()
+                QMessageBox.information(self, "조퇴 처리 완료", "조퇴가 정상적으로 등록되었습니다.")
+                # self.loadData()  # 데이터 새로고침
+                
+            else:
+                QMessageBox.warning(self, "오류", "학생 정보를 찾을 수 없습니다.")
+                print(f"s_id 값: {self.s_id}")
+
+
+        except oci.DatabaseError as e:
+            QMessageBox.critical(self, "데이터베이스 오류", f"오류 내용: {str(e)}")
+
+        finally:
+            cursor.close()
+            connection.close()
+
+        self.studentlogin_window = SAtdMainWindow()
+        self.studentlogin_window.show()
+        self.close()
+
+
+    
+    def btnOutClick(self):
+        """외출 버튼 클릭 시 DB 저장"""
+        try:
+            connection = oci.connect(username, password, f"{host}:{port}/{sid}")
+            cursor = connection.cursor()     
+
+            cursor.execute("SELECT s_no FROM student WHERE s_id = :s_id", {"s_id": self.s_id})
+            result = cursor.fetchone()   
+            
+            if result:
+                s_no = result[0]  # s_no 값 추출
+
+                query = """UPDATE atd
+                        SET out_time = CAST(SYSTIMESTAMP AT TIME ZONE 'UTC' AS TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'Asia/Seoul'
+                        WHERE s_no = :s_no
+                        AND trunc(atd_date) = trunc(sysdate)"""
+
+                cursor.execute(query, {"s_no": s_no})
+                connection.commit()
+                QMessageBox.information(self, "알림", "외출이 정상 처리되었습니다.")
+            else:
+                QMessageBox.warning(self, "알림", "해당 학생을 찾을 수 없습니다.")    
+
+        except oci.DatabaseError as e:
+            QMessageBox.critical(self, "데이터베이스 오류", f"오류 내용: {str(e)}")
+
+        finally:
+            cursor.close()
+            connection.close()
+        
+        self.studentlogin_window = SAtdMainWindow()
+        self.studentlogin_window.show()
+        self.close()
+
+
+    def btnCobClick(self):
+        """복귀 버튼 클릭 시 DB 저장"""
+        try:
+            connection = oci.connect(username, password, f"{host}:{port}/{sid}")
+            cursor = connection.cursor()     
+
+            cursor.execute("SELECT s_no FROM student WHERE s_id = :s_id", {"s_id": self.s_id})
+            result = cursor.fetchone()   
+            
+            if result:
+                s_no = result[0]  # s_no 값 추출
+
+                query = """UPDATE atd
+                        SET return_time = CAST(SYSTIMESTAMP AT TIME ZONE 'UTC' AS TIMESTAMP WITH TIME ZONE) AT TIME ZONE 'Asia/Seoul'
+                        WHERE s_no = :s_no
+                        AND trunc(atd_date) = trunc(sysdate)"""
+
+                cursor.execute(query, {"s_no": s_no})
+                connection.commit()
+                QMessageBox.information(self, "알림", "복귀가 정상 처리되었습니다.")
+            else:
+                QMessageBox.warning(self, "알림", "해당 학생을 찾을 수 없습니다.")    
+
+        except oci.DatabaseError as e:
+            QMessageBox.critical(self, "데이터베이스 오류", f"오류 내용: {str(e)}")
+
+        finally:
+            cursor.close()
+            connection.close()
+
+        self.studentlogin_window = SAtdMainWindow()
+        self.studentlogin_window.show()
+        self.close()
+
+    
+
+
+   
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = SAtdMainWindow()
+    win.show()
+    app.exec_()
--- teamproject/s_login.py
+++ teamproject/s_login.py
@@ -11,12 +11,13 @@ import cx_Oracle as oci
 
 # Oracle 데이터베이스 연결 정보 설정
 sid = 'XE'  # 데이터베이스 SID
-host = '210.119.14.71'  # 데이터베이스 호스트 주소 (외부 접속 시 변경 필요)
+host = 'localhost'  # 데이터베이스 호스트 주소 (외부 접속 시 변경 필요)
 port = 1521  # 데이터베이스 포트 번호
 username = 'attendance'  # 데이터베이스 사용자 이름
 password = '12345'  # 데이터베이스 비밀번호
 basic_msg = 'OO고등학교 출결관리앱 v1.0'
 
+# 학생 로그인 화면
 # 메인 윈도우 클래스 정의
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -25,7 +26,7 @@ class MainWindow(QMainWindow):
 
     # UI 초기화 함수
     def initUI(self):
-        uic.loadUi('./teamproject/학생 로그인 화면.ui', self)
+        uic.loadUi('./s_login.ui', self)
         self.setWindowTitle('학생용 로그인')  # 윈도우 제목 설정
         self.setWindowIcon(QIcon('./image/app01.png'))
 
@@ -45,6 +46,8 @@ class MainWindow(QMainWindow):
         S_ID = self.input_S_ID.text()
         S_PW = self.input_S_PW.text()
 
+        # self.S_ID = self.input_S_ID.text() # 로그인한 ID 변수값에 저장 
+
         if S_ID == '' or S_PW == '':
             QMessageBox.warning(self, '경고', '학번 또는 비밀번호 입력은 필수입니다!')
             return
@@ -53,10 +56,16 @@ class MainWindow(QMainWindow):
             values = (S_ID, S_PW)
             if self.addData(values) == True:
                 QMessageBox.about(self, '로그인 성공!', '어서오세요!')
-                self.studentAttendanceWindow()
+                self.studentAttendanceWindow(values[0])
             else:
                 QMessageBox.about(self, '로그인 실패!', '로그인 실패, 관리자에게 문의하세요.')
         self.clearInput()
+
+    def studentAttendanceWindow(self, s_id):
+        from SAtd_chk import SAtdMainWindow
+        self.studentlogin_window = SAtdMainWindow(s_id)
+        self.studentlogin_window.show()
+        self.close()  # 현재 창을 닫기
 
     # 데이터베이스에서 로그인 정보 확인 함수
     def addData(self, values):
@@ -85,58 +94,7 @@ class MainWindow(QMainWindow):
 
         return isSucceed
     
-    def studentAttendanceWindow(self):
-        self.studentlogin_window = StudentloginWindow()
-        self.studentlogin_window.show()
-        self.close()  # 현재 창을 닫기
-
-
-class StudentloginWindow(QMainWindow):
-    def __init__(self):
-        super(StudentloginWindow, self).__init__()
-        self.initUI()
-
-    def initUI(self):
-        uic.loadUi('./teamproject/로그인 방식.ui', self)
-        self.setWindowTitle('로그인 방식')
-        self.setWindowIcon(QIcon('./image/app01.png'))
-        
-        self.btn_my.clicked.connect(self.studentmyWindow)
-        self.btn_atd.clicked.connect(self.atdcheckWindow)
-        self.btn_eal.clicked.connect(self.atdcheckWindow)
-        self.btn_cob.clicked.connect(self.atdcheckWindow)
-        self.btn_out.clicked.connect(self.atdcheckWindow)
-       
-    def studentmyWindow(self):
-        self.studentmypage_window = StudentMypageWindow()
-        self.studentmypage_window.show()
-
-    def atdcheckWindow(self):
-        self.atdcheckpage_window = AtdCheckWindow()
-        self.atdcheckpage_window.show()
-        
-
-class StudentMypageWindow(QDialog):  # QDialog로 변경
-    def __init__(self):
-        super(StudentMypageWindow, self).__init__()
-        self.initUI()
-
-    def initUI(self):
-        uic.loadUi('./teamproject/마이페이지.ui', self)
-        self.setWindowTitle('마이페이지')
-        self.setWindowIcon(QIcon('./image/app01.png'))
-      
-
-class AtdCheckWindow(QMainWindow): 
-    def __init__(self):
-        super(AtdCheckWindow, self).__init__()
-        self.initUI()
-
-    def initUI(self):
-        uic.loadUi('./teamproject/출석관리,통계.ui', self)
-        self.setWindowTitle('출석관리')
-        self.setWindowIcon(QIcon('./image/app01.png'))
-
+    
 # 프로그램 실행 진입점
 if __name__ == '__main__':
     app = QApplication(sys.argv)
